f354b7a4e9ff37b4d4a5c00eff2b66237851999e
Tiago-0liveira <tfgoliveira@gmail.com>
2024-02-27 19:20:51 +0000

    refactor echo

--- src/b-ins/echo.c
+++ src/b-ins/echo.c
@@ -2,11 +2,11 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+        
+/*                                                    +:+ +:+
 	+:+     */
-/*   By: joaoribe <joaoribe@student.42.fr>          +#+  +:+      
+/*   By: joaoribe <joaoribe@student.42.fr>          +#+  +:+
 	+#+        */
-/*                                                +#+#+#+#+#+  
+/*                                                +#+#+#+#+#+
 	+#+           */
 /*   Created: 2024/02/14 01:21:08 by joaoribe          #+#    #+#             */
 /*   Updated: 2024/02/14 01:21:08 by joaoribe         ###   ########.fr       */
@@ -15,30 +15,29 @@
 
 #include "minishell.h"
 
+
 void	bi_echo(char **av)
 {
-	int	i;
-	int	flag;
+	bool	flag_n;
+	int		i;
 
 	i = 1;
-	flag = 0;
-	if (!av[1])
+	flag_n = false;
+	if (av[i])
 	{
-		printf("\n");
-		return ;
+		while (av[i] && !ft_strncmp(av[i], ECHO_FLAG_N, 2))
+		{
+			flag_n = true;
+			i++;
+		}
+		while (av[i])
+		{
+			ft_putstr_fd(av[i], STDOUT_FILENO);
+			if (av[i + 1])
+				ft_putstr_fd(" ", STDOUT_FILENO);
+			i++;
+		}
 	}
-	if (!ft_strncmp(av[1], "-n", 2))
-	{
-		flag = 1;
-		i++;
-	}
-	while (av[i])
-	{
-		printf("%s", av[i]);
-		i++;
-		if (av[i])
-			printf(" ");
-	}
-	if (!flag)
-		printf("\n");
+	if (!flag_n)
+		ft_putstr_fd("\n", STDOUT_FILENO);
 }
