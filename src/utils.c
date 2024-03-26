e1fb41efd3847355025ad38dce4ff9be3c1ac4ec
Tiago-0liveira <tfgoliveira@gmail.com>
2024-03-26 19:48:07 +0000

    print_command for debugging

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:13:51 by tiagoliv          #+#    #+#             */
-/*   Updated: 2024/03/07 18:50:17 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2024/03/25 17:43:32 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,45 @@ void	free_assign_null(void **ptr)
 	if (*ptr)
 		free(*ptr);
 	*ptr = NULL;
+}
+
+void	print_command(t_command *command)
+{
+	size_t		i;
+	t_redir		*redir;
+	t_command	*tmp;
+	i = 0;
+	if (ft_strlen(command->cmd_name) != 0)
+		DEBUG_MSG("print_command: ");
+	while (command->args && command->args[i])
+	{
+		printf("|%s| ", command->args[i]);
+		i++;
+	}
+	if (ft_strlen(command->cmd_name) != 0)
+		printf("\n");
+	tmp = command;
+	while (tmp)
+	{
+		redir = command->redirs;
+		while (redir)
+		{
+			DEBUG_MSG("redir:|%s| type:", redir->file);
+			if (redir->type)
+			{
+				if (redir->type == RED_IN)
+					printf("RED_IN\n");
+				else if (redir->type == RED_AIN)
+					printf("RED_AIN\n");
+				else if (redir->type == RED_OUT)
+					printf("RED_OUT\n");
+				else if (redir->type == RED_AOUT)
+					printf("RED_AOUT\n");
+			}
+			redir = redir->next;
+		}
+		tmp = tmp->next;
+		if (tmp)
+			DEBUG_MSG("output will be piped to:\n");
+	}
 }
